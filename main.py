a3053295f4ffd1264ec0bcc354d821a2c1fc4755
PrettyScript <JessicaDelcy@gmail.com>
2022-12-12 20:59:18 -0600

    created custom function to convert questions to camel case

---
+++ main.py
@@ -0,0 +1,11 @@
+def convert_question_to_camel_case(question: str):
+    question = question.lower().replace(".", "").split(" ")
+
+    new_str = ""
+    for i in question:
+        new_str += i + "_"
+
+    return new_str
+
+
+print(convert_question_to_camel_case("136. Single Number"))
